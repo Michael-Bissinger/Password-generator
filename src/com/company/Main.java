1135b8147b50c58392b2af1a0f0282c8f10c94cd
Michael Bissinger <yipmicha@web.de>
2018-06-14 16:59:16 +0200

    Generate project-files and basic code-plan

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Password-Generator. Your app to savely generate Passwords.");
+        System.out.println("Please enter your specifications.");
+        System.out.println("Number of characters:");
+        System.out.println("Include numbers? (Yes/No)");
+        System.out.println("Include letters? (Yes/No)");
+
+
+    }
+}
